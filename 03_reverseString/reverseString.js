40519a1cf9ceeb92eb1be6f8746935b954f86f5f
hazelnut6 <hazelpachoco6@gmail.com>
2024-09-07 10:20:51 +0800

    Reverse exercise: finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let wordArray = word.split("");
+    let wordReverse = wordArray.reverse();
+    let result = wordReverse.join("");
 
+    return result;
 };
 
 // Do not edit below this line
